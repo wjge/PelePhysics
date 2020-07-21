1c440705b326ff5b85212e86b60cdea65c618073
Anne Felden <amfelden@lake.lbl.gov>
2020-07-21 03:02:22 -0700

    restore therm and trans for qss specs too

--- Support/Fuego/Pythia/pythia-0.4/packages/fuego/fuego/serialization/mechanisms/QssSpecies.py
+++ Support/Fuego/Pythia/pythia-0.4/packages/fuego/fuego/serialization/mechanisms/QssSpecies.py
@@ -17,24 +17,24 @@ from Entity import Entity
 class QssSpecies(Entity):
 
 
-    #def thermalParametrization(self, type, lowT, highT, locator, parameters):
-        #if type == "NASA":
-            #from NASA import NASA
-            #model = NASA(lowT, highT, locator)
-            #model.parameters = parameters
-        #else:
-            #import pyre
-            #pyre.debug.Firewall.hit("unknown thermal parametrization type '%s'" % type)
-            #return
+    def thermalParametrization(self, type, lowT, highT, locator, parameters):
+        if type == "NASA":
+            from NASA import NASA
+            model = NASA(lowT, highT, locator)
+            model.parameters = parameters
+        else:
+            import pyre
+            pyre.debug.Firewall.hit("unknown thermal parametrization type '%s'" % type)
+            return
 
-        #self.thermo.append(model)
-        #return
+        self.thermo.append(model)
+        return
 
-    #def transParametrization(self, type, EPS, SIG, DIP, POL, ZROT, locator, parameters):
+    def transParametrization(self, type, EPS, SIG, DIP, POL, ZROT, locator, parameters):
         #if type == "0":
-        #from TRANLIN import TRANLIN
-        #model = TRANLIN(EPS, SIG, DIP, POL, ZROT, locator)
-        #model.parameters = parameters
+        from TRANLIN import TRANLIN
+        model = TRANLIN(EPS, SIG, DIP, POL, ZROT, locator)
+        model.parameters = parameters
         #elif type == "1":
         #    model = TRANNL(EPS, SIG, DIP, POL, ZROT, locator)
         #    model.parameters = parameters
@@ -46,16 +46,16 @@ class QssSpecies(Entity):
         #    pyre.debug.Firewall.hit("unknown transport parametrization type '%s'" % type)
         #    return
 
-        #self.trans.append(model)
-        #return
+        self.trans.append(model)
+        return
 
     def __init__(self, id, symbol, locator=None):
         Entity.__init__(self, id, locator)
         self.symbol = symbol
         self.phase = None
         self.composition = []
-        #self.thermo = []
-        #self.trans = []
+        self.thermo = []
+        self.trans = []
         return
 
 
