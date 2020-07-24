1c63941eea5f84061c2f88bf4b65617d7187eefb
Anne Felden <amfelden@lake.lbl.gov>
2020-07-24 01:17:33 -0700

    Cleaning

--- Support/Fuego/Pythia/pythia-0.4/packages/fuego/fuego/fmc/FMC.py
+++ Support/Fuego/Pythia/pythia-0.4/packages/fuego/fuego/fmc/FMC.py
@@ -58,7 +58,6 @@ class FMC(Application):
         print "\nConverting into '%s' format" % output
         lines        = fuego.serialization.save(mechanism, output)
         print "... done (%g sec)" % timer.stop()
-        # stop
 
         print "saving in '%s' '%s' (headers) and '%s'" % (mech_header, save_header, save),
         timer.reset()
