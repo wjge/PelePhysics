fcb484f69760deef3d999c185b9a38481b4cc921
Julia Ream <home@Julias-MacBook-Pro.local>
2020-07-31 13:59:00 -0400

    20 QSS input file and mechanism stuff

--- Support/Fuego/Mechanism/Models/heptane_fc/mechanism.h
+++ Support/Fuego/Mechanism/Models/heptane_fc/mechanism.h
@@ -23,35 +23,29 @@
 #define CO2_ID 9
 #define CO_ID 10
 #define CH2O_ID 11
-#define CH3_ID 12
-#define CH4_ID 13
-#define CH3OH_ID 14
-#define C2H6_ID 15
-#define CH2CO_ID 16
-#define HOCHO_ID 17
-#define CH3O2_ID 18
-#define C2H2_ID 19
-#define C3H6_ID 20
-#define C2H4_ID 21
-#define C3H3_ID 22
-#define C3H4XA_ID 23
-#define C3H5XA_ID 24
-#define NXC3H7_ID 25
+#define CH2GSG_ID 12
+#define CH3_ID 13
+#define CH4_ID 14
+#define CH3OH_ID 15
+#define C2H6_ID 16
+#define CH2CO_ID 17
+#define HOCHO_ID 18
+#define CH3O2_ID 19
+#define C2H2_ID 20
+#define C3H6_ID 21
+#define C2H4_ID 22
+#define CH3CO_ID 23
+#define C3H4XA_ID 24
+#define C3H5XA_ID 25
 #define NXC3H7O2_ID 26
 #define C4H6_ID 27
 #define C4H7_ID 28
 #define C4H8X1_ID 29
-#define PXC4H9_ID 30
-#define PXC4H9O2_ID 31
-#define C5H9_ID 32
-#define C5H10X1_ID 33
-#define C5H11X1_ID 34
-#define C6H12X1_ID 35
-#define C7H15X2_ID 36
-#define NXC7H16_ID 37
+#define C7H15X2_ID 30
+#define NXC7H16_ID 31
 
 #define NUM_ELEMENTS 4
-#define NUM_SPECIES 38
+#define NUM_SPECIES 32
 #define NUM_REACTIONS 218
 
 #define NUM_FIT 4
@@ -60,6 +54,6 @@
 /* version */
 /* $Id$ */
 
-/* Generated automatically by CPickler on Wed Jul 29 01:38:40 2020 */
+/* Generated automatically by CPickler on Fri Jul 31 13:58:10 2020 */
 
 /* End of file  */
