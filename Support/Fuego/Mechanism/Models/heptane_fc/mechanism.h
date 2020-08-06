25d79db4dfd7529cadd50a1fd94a54919265e35b
Anne Felden <amfelden@lake.lbl.gov>
2020-08-06 01:55:53 -0700

    Regenerate heptane_fc. No more qss in this model

--- Support/Fuego/Mechanism/Models/heptane_fc/mechanism.h
+++ Support/Fuego/Mechanism/Models/heptane_fc/mechanism.h
@@ -20,32 +20,52 @@
 #define O2_ID 6
 #define HO2_ID 7
 #define H2O2_ID 8
-#define CO2_ID 9
-#define CO_ID 10
-#define CH2O_ID 11
-#define CH2GSG_ID 12
-#define CH3_ID 13
-#define CH4_ID 14
-#define CH3OH_ID 15
-#define C2H6_ID 16
-#define CH2CO_ID 17
-#define HOCHO_ID 18
-#define CH3O2_ID 19
-#define C2H2_ID 20
-#define C3H6_ID 21
-#define C2H4_ID 22
-#define CH3CO_ID 23
-#define C3H4XA_ID 24
-#define C3H5XA_ID 25
-#define NXC3H7O2_ID 26
-#define C4H6_ID 27
-#define C4H7_ID 28
-#define C4H8X1_ID 29
-#define C7H15X2_ID 30
-#define NXC7H16_ID 31
+#define CH_ID 9
+#define HCO_ID 10
+#define CH2_ID 11
+#define CO2_ID 12
+#define CO_ID 13
+#define CH2O_ID 14
+#define CH2GSG_ID 15
+#define CH3_ID 16
+#define CH3O_ID 17
+#define CH4_ID 18
+#define CH3OH_ID 19
+#define C2H6_ID 20
+#define C2H5_ID 21
+#define CH2CO_ID 22
+#define HOCHO_ID 23
+#define CH3O2_ID 24
+#define CH3O2H_ID 25
+#define C2H2_ID 26
+#define HCCO_ID 27
+#define C2H3_ID 28
+#define CH2CHO_ID 29
+#define C3H6_ID 30
+#define C2H4_ID 31
+#define C2H5O_ID 32
+#define CH3CO_ID 33
+#define C2H5O2_ID 34
+#define C3H2_ID 35
+#define C3H3_ID 36
+#define C3H4XA_ID 37
+#define C3H5XA_ID 38
+#define NXC3H7_ID 39
+#define NXC3H7O2_ID 40
+#define C4H6_ID 41
+#define C4H7_ID 42
+#define C4H8X1_ID 43
+#define PXC4H9_ID 44
+#define PXC4H9O2_ID 45
+#define C5H9_ID 46
+#define C5H10X1_ID 47
+#define C5H11X1_ID 48
+#define C6H12X1_ID 49
+#define C7H15X2_ID 50
+#define NXC7H16_ID 51
 
 #define NUM_ELEMENTS 4
-#define NUM_SPECIES 32
+#define NUM_SPECIES 52
 #define NUM_REACTIONS 218
 
 #define NUM_FIT 4
@@ -54,6 +74,6 @@
 /* version */
 /* $Id$ */
 
-/* Generated automatically by CPickler on Fri Jul 31 13:58:10 2020 */
+/* Generated automatically by CPickler on Thu Aug  6 01:55:00 2020 */
 
 /* End of file  */
