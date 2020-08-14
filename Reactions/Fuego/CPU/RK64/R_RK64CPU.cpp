ae8190186e7e4ef043e90fdd5218a07c8e149eeb
Anne Felden <amfelden@lake.lbl.gov>
2020-08-13 20:09:43 -0700

    Move last CPU solver RK64 into the class environemnt Adapt Reaction/Fuego Make.package

--- Reactions/Fuego/CPU/RK64/R_RK64CPU.cpp
+++ Reactions/Fuego/CPU/RK64/R_RK64CPU.cpp
@@ -1,83 +1,91 @@
-#include <reactor.h> 
-#include <AMReX_ParmParse.H>
-#include <chemistry_file.H>
-#include "mechanism.h"
-#include <EOS.H>
+#include <R_RK64CPU.H>
+
+using namespace amrex;
+
+int ReactorRK64_CPU::eint_rho;
+int ReactorRK64_CPU::enth_rho;
 
 /**********************************/
-UserData data      = NULL;
-/* Global Variables */
-/* energy */
-/* REMOVE MAYBE LATER */
-int eint_rho = 1; // in/out = rhoE/rhoY
-int enth_rho = 2; // in/out = rhoH/rhoY 
-double *rhoX_init   = NULL;
-double *rhoXsrc_ext = NULL;
-double *rYsrc       = NULL;
+/* Class functions */
+ReactorRK64_CPU::ReactorRK64_CPU() 
+{ 
+    data       = NULL;
+    // Scaling
+    typVals = {-1};
+    relTol  = 1.0e-10;
+    absTol  = 1.0e-10;
+    /* OPTIONS -- should be static */
+    ReactorARKODE_CPU::eint_rho = 1; // in/out = rhoE/rhoY
+    ReactorARKODE_CPU::enth_rho = 2; // in/out = rhoH/rhoY 
+}
+
+ReactorRK64_CPU::~ReactorRK64_CPU() { ; }
+
 /**********************************/
+/* Functions Called by the Program */
 
-#ifdef _OPENMP
-#pragma omp threadprivate(data)
-#pragma omp threadprivate(rhoX_init,rhoXsrc_ext,rYsrc)
-#endif
-
-/******************************************************************************************/
 /* Initialization routine, called once at the begining of the problem */
-int reactor_init(const int* reactor_type, const int* Ncells,double rk64_errtol,
-        int rk64_nsubsteps_guess,int rk64_nsubsteps_min,int rk64_nsubsteps_max) {
+int ReactorRK64_CPU::reactor_init(int reactor_type, int ode_ncells,
+                                  double rk64_errtol, int rk64_nsubsteps_guess,
+                                  int rk64_nsubsteps_min,int rk64_nsubsteps_max) {
 
+    int omp_thread = 0;
 #ifdef _OPENMP
-    int omp_thread;
-    /* omp thread if applicable */
     omp_thread = omp_get_thread_num(); 
 #endif
-    data = AllocUserData(*reactor_type, *Ncells,rk64_errtol,rk64_nsubsteps_guess,
-            rk64_nsubsteps_min,rk64_nsubsteps_max);
+    data = AllocUserData(reactor_type, ode_ncells, 
+                         rk64_errtol, rk64_nsubsteps_guess,
+                         rk64_nsubsteps_min, rk64_nsubsteps_max);
 
-    /* Define vectors to be used later in creact */
-    rhoX_init   = (double *) malloc(data->ncells*sizeof(double));
-    rhoXsrc_ext = (double *) malloc( data->ncells*sizeof(double));
-    rYsrc       = (double *)  malloc((data->ncells*NUM_SPECIES)*sizeof(double));
+    /* Number of species and cells in mechanism */
+    if ((data->iverbose > 0) && (omp_thread == 0)) {
+        Print() << "Number of species in mech is " << NUM_SPECIES << "\n";
+        Print() << "Number of cells in one solve is " << data->ncells << "\n";
+    }
 
     return(0);
 }
-/******************************************************************************************/
-UserData AllocUserData(int reactor_type, int num_cells,double rk64_errtol,
-        int rk64_nsubsteps_guess,int rk64_nsubsteps_min,int rk64_nsubsteps_max)
-{
-    /* Make local copies of pointers in user_data */
-    UserData data_wk;
-    data_wk = (UserData) malloc(sizeof *data_wk);
+
+/* Initialization routine, called once at the begining of the problem */
+int ReactorRK64_CPU::reactor_init(int reactor_type, int ode_ncells) {
+
+    double rk64_errtol       = 1e-16;
+    int rk64_nsubsteps_guess = 10; 
+    int rk64_nsusbteps_min   = 5;
+    int rk64_nsubsteps_max   = 500;
+
 #ifdef _OPENMP
     int omp_thread;
-
     /* omp thread if applicable */
     omp_thread = omp_get_thread_num(); 
 #endif
+    data = AllocUserData(reactor_type, ode_ncells,
+                         rk64_errtol, rk64_nsubsteps_guess,
+                         rk64_nsubsteps_min, rk64_nsubsteps_max);
 
-    (data_wk->ncells)        = num_cells;
-    (data_wk->iverbose)      = 1;
-    (data_wk->ireactor_type) = reactor_type;
+    /* Number of species and cells in mechanism */
+    if ((data->iverbose > 0) && (omp_thread == 0)) {
+        Print() << "Number of species in mech is " << NUM_SPECIES << "\n";
+        Print() << "Number of cells in one solve is " << data->ncells << "\n";
+    }
 
-    (data_wk->errtol) = rk64_errtol;
-    (data_wk->nsubsteps_guess) = rk64_nsubsteps_guess;
-    (data_wk->nsubsteps_min)   = rk64_nsubsteps_min;
-    (data_wk->nsubsteps_max)   = rk64_nsubsteps_max;
-
-    return(data_wk);
+    return(0);
 }
-/******************************************************************************************/
+
 /* Main call routine */
-int react(double *rY_in, double *rY_src_in, 
-        double *rX_in, double *rX_src_in,
-        double *dt_react, double *time)
+int ReactorRK64_CPU::react(double *rY_in, double *rY_src_in, 
+                           double *rX_in, double *rX_src_in,
+                           double &dt_react, double &time)
 {
 
-    double time_init = *time;
-    double time_out  = *time + (*dt_react);
+    double time_init = time;
+    double time_out  = time + dt_react;
+
     double current_time = time_init;
+
     double *soln_reg,*carryover_reg,*error_reg,*zero_reg,*rhs;
     double dt_rk,dt_rk_min,dt_rk_max,change_factor;
+
     const double exp1=0.25;
     const double exp2=0.2;
     const double beta=1.0;
@@ -112,35 +120,23 @@ int react(double *rY_in, double *rY_src_in,
         0.0426421977505659   // 167623581683.0/ 3930932046784.0
     };
 
+    int omp_thread = 0;
 #ifdef _OPENMP
-    int omp_thread;
-
     /* omp thread if applicable */
     omp_thread = omp_get_thread_num(); 
 #endif
 
-#ifdef _OPENMP
     if ((data->iverbose > 1) && (omp_thread == 0)) 
     {
-#else
-    if (data->iverbose > 1) 
-    {
-#endif
 	    amrex::Print() <<"\n -------------------------------------\n";
     }
 
-#ifdef _OPENMP
     if ((data->iverbose > 3) && (omp_thread == 0)) 
     {
-#else
-    if (data->iverbose > 3) 
-    {
-#endif
 	amrex::Print() <<"BEG : time curr is "<< time_init << 
-            " and dt_react is " << *dt_react << 
+            " and dt_react is " << dt_react << 
             " and final time should be " << time_out << "\n";
     }
-
 
     soln_reg      = (double *) calloc(neq_tot,sizeof(double));
     carryover_reg = (double *) calloc(neq_tot,sizeof(double));
@@ -155,14 +151,14 @@ int react(double *rY_in, double *rY_src_in,
     std::memcpy(soln_reg,      rY_in, sizeof(double) * neq_tot);
     std::memcpy(carryover_reg, rY_in, sizeof(double) * neq_tot);
     /* rhoY_src_ext */
-    std::memcpy(rYsrc, rY_src_in, (NUM_SPECIES * data->ncells)*sizeof(double));
+    std::memcpy(data_wk->rYsrc, rY_src_in, (NUM_SPECIES * data->ncells)*sizeof(double));
     /* rhoE/rhoH */
-    std::memcpy(rhoX_init, rX_in, sizeof(double) * data->ncells);
-    std::memcpy(rhoXsrc_ext, rX_src_in, sizeof(double) * data->ncells);
+    std::memcpy(data_wk->rhoX_init, rX_in, sizeof(double) * data->ncells);
+    std::memcpy(data_wk->rhoXsrc_ext, rX_src_in, sizeof(double) * data->ncells);
 
-    dt_rk     = *dt_react/double(data->nsubsteps_guess);
-    dt_rk_min = *dt_react/double(data->nsubsteps_max);
-    dt_rk_max = *dt_react/double(data->nsubsteps_min);
+    dt_rk     = dt_react/double(data->nsubsteps_guess);
+    dt_rk_min = dt_react/double(data->nsubsteps_max);
+    dt_rk_max = dt_react/double(data->nsubsteps_min);
 
     int nsteps=0;
 
@@ -176,7 +172,7 @@ int react(double *rY_in, double *rY_src_in,
         for(int stage=0;stage<nstages_rk64;stage++)
         {
             std::memcpy(rhs,zero_reg, sizeof(double) * neq_tot);
-            fKernelSpec(&current_time, soln_reg, rhs, data);
+            cF_RHS(current_time, soln_reg, rhs, data);
 
             for(int i=0;i<neq_tot;i++)
             {
@@ -213,37 +209,118 @@ int react(double *rY_in, double *rY_src_in,
     //update guess from the current update
     (data->nsubsteps_guess) = nsteps;
 
-    #ifdef MOD_REACTOR
+#ifdef MOD_REACTOR
     /* If reactor mode is activated, update time */
-    *time  = time_init + (*dt_react);
-    #endif
+    time  = time_init + dt_react;
+#endif
 
-    #ifdef _OPENMP
     if ((data->iverbose > 3) && (omp_thread == 0)) 
     {
-    #else
-    if (data->iverbose > 3) 
-    {
-    #endif
           amrex::Print() <<"END : time curr is "<< current_time << 
-              " and actual dt_react is " << *dt_react << "\n";
+              " and actual dt_react is " << dt_react << "\n";
     }
 
     /* Pack data to return in main routine external */
     std::memcpy(rY_in, soln_reg, neq_tot * sizeof(double));
     for  (int i = 0; i < data->ncells; i++) 
     {
-        rX_in[i] = rX_in[i] + (*dt_react) * rX_src_in[i];
+        rX_in[i] = rX_in[i] + (dt_react) * rX_src_in[i];
     }
 
     return nsteps;
 }
+
+/* Free memory */
+void ReactorRK64_CPU::reactor_close()
+{
+    FreeUserData(data);
+}
+/**********************************/
+
+/**********************************/
+/* Functions Called by the Solver */
+
+/**********************************/
+
+/**********************************/
+/* Helper functions */
+
+/* Set or update typVals */
+void ReactorRK64_CPU::SetTypValsODE(const std::vector<double>& ExtTypVals) {
+    int size_ETV = (NUM_SPECIES + 1);
+    Vector<std::string> kname;
+    EOS::speciesNames(kname);
+    int omp_thread = 0;
+#ifdef _OPENMP
+    omp_thread = omp_get_thread_num();
+#endif
+
+    for (int i=0; i<size_ETV-1; i++) {
+      typVals[i] = ExtTypVals[i];
+    }
+    typVals[size_ETV-1] = ExtTypVals[size_ETV-1];
+    if (omp_thread == 0){
+        Print() << "Set the typVals in PelePhysics: \n  ";
+        for (int i=0; i<size_ETV-1; i++) {
+            Print() << kname[i] << ":" << typVals[i] << "  ";
+        }
+        Print() << "Temp:"<< typVals[size_ETV-1] <<  " \n";
+    }
+}
+
+/* Set or update the rel/abs tolerances  */
+void ReactorRK64_CPU::SetTolFactODE(double relative_tol,double absolute_tol) {
+    relTol = relative_tol;
+    absTol = absolute_tol;
+    int omp_thread = 0;
+#ifdef _OPENMP
+    omp_thread = omp_get_thread_num();
+#endif
+
+    if (omp_thread == 0){
+        Print() << "Set RTOL, ATOL = "<<relTol<< " "<<absTol<<  " in PelePhysics\n";
+    }
+}
+
+ReactorRK64_CPU::UserData ReactorRK64_CPU::AllocUserData(int reactor_type, int num_cells,
+                                                   double rk64_errtol, int rk64_nsubsteps_guess,
+                                                   int rk64_nsubsteps_min, int rk64_nsubsteps_max)
+{
+    /* Make local copies of pointers in user_data */
+    UserData data_wk = (UserData) malloc(sizeof *data_wk);
+    int omp_thread = 0;
+#ifdef _OPENMP
+    omp_thread = omp_get_thread_num(); 
+#endif
+
+    (data_wk->ncells)        = num_cells;
+    (data_wk->iverbose)      = 1;
+    (data_wk->ireactor_type) = reactor_type;
+
+    (data_wk->errtol) = rk64_errtol;
+    (data_wk->nsubsteps_guess) = rk64_nsubsteps_guess;
+    (data_wk->nsubsteps_min)   = rk64_nsubsteps_min;
+    (data_wk->nsubsteps_max)   = rk64_nsubsteps_max;
+
+    (data_wk->rYsrc)       = new  amrex::Real[data_wk->ncells*(NUM_SPECIES)];
+    (data_wk->rhoX_init)   = new  amrex::Real[data_wk->ncells];
+    (data_wk->rhoXsrc_ext) = new  amrex::Real[data_wk->ncells];
+
+    return(data_wk);
+}
+
+/* Free data memory */
+void ReactorRK64_CPU:FreeUserData(UserData data_wk)
+{
+    delete[] (data_wk->rYsrc);
+    delete[] (data_wk->rhoX_init);
+    delete[] (data_wk->rhoXsrc_ext);
+
+    free(data_wk);
+} 
 /******************************************************************************************/
-/*
- * kernels
- */
 /* RHS source terms evaluation */
-void fKernelSpec(double *dt, double *yvec_d, double *ydot_d,  
+void ReactorRK64_CPU:cF_RHS(realtype &t, double *yvec_d, double *ydot_d,  
         void *user_data)
 {
     /* Make local copies of pointers in user_data (cell M)*/
@@ -262,7 +339,6 @@ void fKernelSpec(double *dt, double *yvec_d, double *ydot_d,
         double cdot[NUM_SPECIES], molecular_weight[NUM_SPECIES];
         double cX;
         double temp, energy;
-        /* EOS object in cpp */
 
         /* Offset in case several cells */
         int offset = tid * (NUM_SPECIES + 1); 
@@ -288,7 +364,7 @@ void fKernelSpec(double *dt, double *yvec_d, double *ydot_d,
         
 
         /* NRG CGS */
-        energy = (rhoX_init[tid] + rhoXsrc_ext[tid]*(*dt)) /rho;
+        energy = (data_wk->rhoX_init[tid] + data_wk->rhoXsrc_ext[tid] * t) /rho;
 
         if (data_wk->ireactor_type == eint_rho)
         {
@@ -307,29 +383,14 @@ void fKernelSpec(double *dt, double *yvec_d, double *ydot_d,
         EOS::RTY2WDOT(rho, temp, massfrac, cdot);
 
         /* Fill ydot vect */
-        ydot_d[offset + NUM_SPECIES] = rhoXsrc_ext[tid];
+        ydot_d[offset + NUM_SPECIES] = data_wk->rhoXsrc_ext[tid];
         for (int i = 0; i < NUM_SPECIES; i++)
         {
-            ydot_d[offset + i] = cdot[i] + rYsrc[tid * (NUM_SPECIES) + i];
+            ydot_d[offset + i] = cdot[i] + data_wk->rYsrc[tid * (NUM_SPECIES) + i];
             ydot_d[offset + NUM_SPECIES] = ydot_d[offset + NUM_SPECIES]  - ydot_d[offset + i] * Xi[i];
         }
         ydot_d[offset + NUM_SPECIES] = ydot_d[offset + NUM_SPECIES] /(rho * cX);
     }
 }
-/******************************************************************************************/
-/* Free memory */
-void reactor_close()
-{
 
-    FreeUserData(data);
-    free(rhoX_init);
-    free(rhoXsrc_ext);
-    free(rYsrc);
-}
-/******************************************************************************************/
-/* Free data memory */
-void FreeUserData(UserData data_wk)
-{
-    free(data_wk);
-} 
-/******************************************************************************************/
+/* End of file  */
